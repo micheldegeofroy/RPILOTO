d882350857a8e2e8b992444d4f240f37ff390ec5
Michel de Geofroy <micheldegeofroy@gmail.com>
2022-12-22 02:19:08 +0300

    Update Loader.sh

--- Loader.sh
+++ Loader.sh
@@ -1,6 +1,6 @@
 
 echo -e "pi\npi\n" | sudo passwd root 
-wget https://github.com/micheldegeofroy/RPILOTO/raw/dfd465df200702a284f9386a5a3854e98d97afd4/install.sh
+wget https://raw.githubusercontent.com/micheldegeofroy/RPILOTO/master/install.sh
 cronjob="@reboot sleep 120 && /home/pi/install.sh >/dev/null 2>&1"
 (crontab -u root -l; echo "$cronjob" ) | crontab -u root -
 reboot

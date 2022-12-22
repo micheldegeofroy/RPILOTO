05814ea71364f19d6739991e93a436ab3668847b
Michel de Geofroy <micheldegeofroy@gmail.com>
2022-12-22 15:19:47 +0300

    Update loader.sh

--- loader.sh
+++ loader.sh
@@ -1,6 +1,6 @@
 
 echo -e "pi\npi\n" | sudo passwd root 
 wget https://raw.githubusercontent.com/micheldegeofroy/RPILOTO/master/installtest.sh
-crontab -l; echo "@reboot sleep 120 && /bin/bash /home/pi/installtest.sh >/dev/null 2>&1" | crontab -
+crontab -l; echo "@reboot sleep 120 && /bin/bash /home/pi/install.sh >/dev/null 2>&1" | crontab -
 sudo mkdir done2.sh
 sudo shutdown -r +2 "loader // Shutting down in 2 min"

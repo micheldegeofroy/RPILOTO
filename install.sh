a7f391b3e09164635a94a0a4512bf00fbb47e786
Michel de Geofroy <micheldegeofroy@gmail.com>
2022-12-22 17:46:40 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -54,38 +54,17 @@
 #bash install.sh
 #
 # ###############################
-# Update the software sources
+# Remove Cronjob
 # ###############################
 
 crontab -u pi -r
 
+# ###############################
+# Update the software sources
+# ###############################
+
 apt update -y
 apt upgrade -y
-
-# ###############################
-# Fix Language Local
-# ###############################
-
-rm /etc/environment
-touch /etc/environment
-
-echo "LANGUAGE=en_US" >> /etc/environment
-echo "LC_ALL=en_US" >> /etc/environment
-echo "LANG=en_US" >> /etc/environment
-echo "LC_TYPE=en_US" >> /etc/environment
-
-rm /etc/default/locale
-touch /etc/default/locale
-
-echo "LANG=en_US.UTF-8" >> /etc/default/locale
-echo "LC_CTYPE=en_US.UTF-8" >> /etc/default/locale
-echo "LC_MESSAGES=en_US.UTF-8" >> /etc/default/locale
-echo "LC_ALL=en_US.UTF-8" >> /etc/default/locale
-
-localedef -f UTF-8 -i en_US en_US.UTF-8 
-
-sed -i 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
-sed -i 's/en_GB.UTF-8 UTF-8/# en_GB.UTF-8 UTF-8/g' /etc/locale.gen 
 
 # ###############################
 # Web Interface

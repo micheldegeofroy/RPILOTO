65dddaeab12b37843595e9532cdad784a4256ee1
Michel de Geofroy <micheldegeofroy@gmail.com>
2022-12-22 23:38:46 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,32 +15,32 @@ apt upgrade -y
 # Fix Language Local
 # ###############################
 
-rm /etc/environment
-touch /etc/environment
+sudo rm /etc/environment
+sudo touch /etc/environment
 
-echo "LANGUAGE=en_US" >> /etc/environment
-echo "LC_ALL=en_US" >> /etc/environment
-echo "LANG=en_US" >> /etc/environment
-echo "LC_TYPE=en_US" >> /etc/environment
+sudo echo "LANGUAGE=en_US" >> /etc/environment
+sudo echo "LC_ALL=en_US" >> /etc/environment
+sudo echo "LANG=en_US" >> /etc/environment
+sudo echo "LC_TYPE=en_US" >> /etc/environment
 
-rm /etc/default/locale
-touch /etc/default/locale
+sudo rm /etc/default/locale
+sudo touch /etc/default/locale
 
-echo "LANG=en_US.UTF-8" >> /etc/default/locale
-echo "LC_CTYPE=en_US.UTF-8" >> /etc/default/locale
-echo "LC_MESSAGES=en_US.UTF-8" >> /etc/default/locale
-echo "LC_ALL=en_US.UTF-8" >> /etc/default/locale
+sudo echo "LANG=en_US.UTF-8" >> /etc/default/locale
+sudo echo "LC_CTYPE=en_US.UTF-8" >> /etc/default/locale
+sudo echo "LC_MESSAGES=en_US.UTF-8" >> /etc/default/locale
+sudo echo "LC_ALL=en_US.UTF-8" >> /etc/default/locale
 
-localedef -f UTF-8 -i en_US en_US.UTF-8 
+sudo localedef -f UTF-8 -i en_US en_US.UTF-8 
 
-sed -i 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
-sed -i 's/en_GB.UTF-8 UTF-8/# en_GB.UTF-8 UTF-8/g' /etc/locale.gen
+sudo sed -i 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
+sudo sed -i 's/en_GB.UTF-8 UTF-8/# en_GB.UTF-8 UTF-8/g' /etc/locale.gen
 
 # ###############################
 # Download Install File
 # ###############################
 
-wget "https://raw.githubusercontent.com/micheldegeofroy/RPILOTO/master/install.sh"
+sudo wget "https://raw.githubusercontent.com/micheldegeofroy/RPILOTO/master/install.sh"
 
 wait
 

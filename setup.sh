fb3dcd4e62facfdd990a2a1f57981a24be492732
Michel de Geofroy <micheldegeofroy@gmail.com>
2022-12-22 18:12:34 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,3 +22,22 @@ localedef -f UTF-8 -i en_US en_US.UTF-8
 
 sed -i 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
 sed -i 's/en_GB.UTF-8 UTF-8/# en_GB.UTF-8 UTF-8/g' /etc/locale.gen
+
+# ###############################
+# Download Install File
+# ###############################
+
+wget https://raw.githubusercontent.com/micheldegeofroy/RPILOTO/master/install.sh
+
+# ###############################
+# Cron @ Reboot to Launch Install
+# ###############################
+
+crontab -l; echo "@reboot sleep 120 && /bin/bash /home/pi/install.sh >/dev/null 2>&1" | crontab -
+
+# ###############################
+# Other Tasks
+# ###############################
+
+sudo touch done2.sh
+sudo shutdown -r +2 "loader // Shutting down in 2 min"

d673d76a59d944646fe9966251d6b6c74223f6b1
Michel de Geofroy <micheldegeofroy@gmail.com>
2022-12-22 15:04:21 +0300

    Update and rename Loader.sh to loader.sh

--- loader.sh
+++ loader.sh
@@ -2,4 +2,5 @@
 echo -e "pi\npi\n" | sudo passwd root 
 wget https://raw.githubusercontent.com/micheldegeofroy/RPILOTO/master/installtest.sh
 crontab -l; echo "@reboot sleep 120 && /bin/bash /home/pi/installtest.sh >/dev/null 2>&1" | crontab -
-sudo shutdown -r +2 "Loader // Shutting down in 2 min"
+sudo mkdir done2.sh
+sudo shutdown -r +2 "loader // Shutting down in 2 min"

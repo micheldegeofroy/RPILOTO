e2a36aea073bed4cbe3ae625cddbabad82e3de7e
Michel de Geofroy <micheldegeofroy@gmail.com>
2022-12-23 23:18:12 +0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -198,6 +198,7 @@ sudo apt purge pip -y
 
 #sudo rm -r /usr/local/lib/python3.9
 sudo rm -r /usr/lib/python3
+sudo rm -r file
 #sudo rm -r /usr/lib/php
 #sudo rm -r /usr/lib/apache2
 #sudo rm -r /usr/lib/python2.7

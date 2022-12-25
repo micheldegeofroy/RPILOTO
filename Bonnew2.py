4a32873994922bad9d3f1d24cb5a8de1f5863820
Michel de Geofroy <micheldegeofroy@gmail.com>
2022-12-26 01:19:43 +0300

    Create Bonnew2.py

---
+++ Bonnew2.py
@@ -0,0 +1,132 @@
+import time
+import random
+import datetime
+import subprocess
+import re
+import os
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Replace this with your own bot token
+TOKEN = "5564114282:AAGSjjJkjNH7RB-4dUH-aJW1pMmquFEq-m8"
+
+# Replace with the chat ID of the user you want to send the message to
+ADMIN_ID = 90423887
+
+# Replace with the message you want to send
+MESSAGE = "BTC Loto BOT is back online!"
+
+bot = telegram.Bot(token=TOKEN)
+bot.send_message(chat_id=ADMIN_ID, text=MESSAGE)
+
+def read_chat_ids():
+    with open('chat_ids.txt', 'r') as f:
+        chat_ids = [int(line.strip()) for line in f]
+    return chat_ids
+
+def message_received(update, context):
+    # Get the chat ID of the sender
+    chat_id = update.message.chat_id
+    # Read the list of allowed chat IDs
+    allowed_chat_ids = read_chat_ids()
+    # Check if the chat ID is in the list of allowed chat IDs
+    if chat_id in allowed_chat_ids:
+        # Get the message text and command
+        command = update.message.text
+        print('Got command: %s' % command)
+        
+        if command == '/who':
+            run = subprocess.run(["whoami"], capture_output=True)
+            context.bot.send_message(chat_id, run.stdout.decode())
+        elif command == '/model':
+            run = subprocess.run(["cat", "/proc/device-tree/model"], capture_output=True)
+            context.bot.send_message(chat_id, run.stdout.decode())
+        elif command == '/uptime':
+            run = subprocess.run(["uptime","-p"], capture_output= True)
+            context.bot.sendMessage(chat_id, run.stdout.decode()[2:])
+        elif command == '/where':
+            run = subprocess.run(["""curl https://extreme-ip-lookup.com/json/?key=ACJdcEKqljZrmlXp1GZA | jq -r '.country' | tr '[:lower:]' '[:upper:]'"""],shell=True,capture_output= True) 
+            context.bot.sendMessage(chat_id, run.stdout.decode())
+        elif command == '/hd':
+            run = subprocess.run(["""df -h | grep "/$" | awk '{ print $5 }'"""], shell=True, capture_output= True)
+            context.bot.sendMessage(chat_id, text = "SD HD Used: "+ run.stdout.decode('utf-8'))
+        elif command == '/volts':
+            run = subprocess.run(["""vcgencmd measure_volts $id | awk '{print substr($0, 6, length($0) - 9),"V"}'"""], shell=True, capture_output= True)
+            context.bot.sendMessage(chat_id, text = "Volts Used: "+run.stdout.decode('utf-8' ))
+        elif command == '/speed':
+            run = subprocess.run(['speedtest-cli --simple'], shell=True, capture_output= True)
+            context.bot.sendMessage(chat_id, text = "Speed Test: "+run.stdout.decode('utf-8'))
+        elif command == '/cpughz':
+            run = subprocess.run(["""vcgencmd measure_clock arm | awk ' BEGIN { FS="=" } ; { printf( $2 / 1000000000) } '"""], shell=True, capture_output= True)
+            context.bot.sendMessage(chat_id,text = "CPU : " + run.stdout.decode('utf-8')[:3] + "GHz")
+        elif command == '/cpu':
+            run = subprocess.run(["""vmstat 1 2 | tail -1 | awk '{print $13}'"""], shell=True, capture_output= True)
+            context.bot.sendMessage(chat_id,text = "CPU% : " + run.stdout.decode('utf-8')[:3] + "%")
+        elif command == '/wanip':
+            run = subprocess.run(['curl ifconfig.co'], shell=True, capture_output= True)
+            context.bot.sendMessage(chat_id,text = "Public IP: "+run.stdout.decode('utf-8'))
+        elif command == '/lanip':
+            run = subprocess.run(['hostname -I'], shell=True, capture_output= True)
+            context.bot.sendMessage(chat_id,text = "Local IP: "+run.stdout.decode('utf-8'))
+        elif command == '/hdex':
+            run = subprocess.run(["""df -h | grep "/dev/sd" | awk '{print$5}'"""], shell=True, capture_output= True)
+            context.bot.sendMessage(chat_id, text = "External HD Used: "+ run.stdout.decode('utf-8'))
+        elif command == '/htop':
+            run = subprocess.run(['ps -e --sort -%mem | head -10'], shell=True, capture_output= True)
+            context.bot.sendMessage(chat_id,text = "Proccese Running: "+run.stdout.decode('utf-8'))
+        elif command == '/temp':
+            run = subprocess.run(['cat /sys/class/thermal/thermal_zone0/temp'], shell=True, capture_output= True)
+            context.bot.sendMessage(chat_id,text = "Temperature: " + str(int(run.stdout.decode('utf-8'))/1000)[:-4] + "C")
+        elif command == '/sync': 
+            run = subprocess.run(["""bitcoin-cli -rpcuser=USER -rpcpassword=PASS getblockchaininfo 2> /dev/null  | jq -r '.verificationprogress' | awk '{print 100 * $1}'"""], shell=True, capture_output= True)
+            context.bot.sendMessage(chat_id,text = "Blockchain Sync is at: " + run.stdout.decode('utf-8')[:4] + "%")
+        elif re.search("magnet:", command):
+            run = subprocess.run(["transmission-remote -a '%s'  " %  command  ], shell=True, capture_output = True)
+            print(run.stdout)
+            context.bot.sendMessage(chat_id, "Well, something happened, maybe good, maybe shit")
+        elif re.search("ping", command):
+            run = subprocess.run([ command + """ -c 3 | awk -F ' ' '{print substr($7, 1, length($1) 0)}' | sed -e 's/\(data.\|packet\)//'"""], shell=True, capture_output = True)
+            context.bot.sendMessage(chat_id, "Ping" + run.stdout.decode('utf-8'))
+            print(run.stdout)
+        elif re.search("sudo", command):
+            run = subprocess.run([command], shell=True, capture_output = True)
+            context.bot.sendMessage(chat_id, "Sudo " + run.stdout.decode('utf-8'))
+            print(run.stdout)
+        elif command == '/Fuck':      
+            context.bot.sendMessage(chat_id, "Are you from NYC ?")
+        elif command == '/help':
+            context.bot.sendMessage(chat_id, "/start /shutdown /startminer /stopminer /fanon /fanoff /ledon /ledoff /ping /sudo /help /htop /sync /model /uptime /where /who /hd /hdex /volts /speed /cpughz /cpu /wanip /lanip /temp /reboot ")
+        elif command == '/start':
+            context.bot.sendMessage(chat_id, "/start /shutdown /startminer /stopminer /fanon /fanoff /ledon /ledoff /ping /sudo /help /htop /sync /model /uptime /where /who /hd /hdex /volts /speed /cpughz /cpu /wanip /lanip /temp /reboot ")
+        elif command == '/reboot':
+            context.bot.sendMessage(chat_id,'Rebooting Now ')
+            os.system('sudo reboot')
+        elif command == '/shutdown':
+            context.bot.sendMessage(chat_id,'Shutting Down Now !')
+            os.system('sudo shutdown')
+        elif command == '/startminer':
+            context.bot.sendMessage(chat_id,'Miner Started')
+            os.system('sudo systemctl start bfgminer.service')
+        elif command == '/stopminer':
+            context.bot.sendMessage(chat_id,'Miner Stopped')
+            os.system('sudo systemctl stop bfgminer.service')
+        else:
+            context.bot.send_message(chat_id, 'Try /help')
+    else:
+        # Send a message to the user telling them to contact the admin
+        context.bot.send_message(chat_id=chat_id, text='Unauthorized')
+
+# Create the Updater and pass it the bot's token.
+updater = Updater(TOKEN, use_context=True)
+
+# Get the dispatcher to register handlers
+dispatcher = updater.dispatcher
+
+# Add a message handler for regular messages
+dispatcher.add_handler(MessageHandler(Filters.text, message_received))
+
+# Start the bot
+updater.start_polling()
+
+# Run the bot indefinitely
+updater.idle()

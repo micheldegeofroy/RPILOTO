fbd20c66b8b4bd347350cc3177baaddc0cdc7f36
Michel de Geofroy <micheldegeofroy@gmail.com>
2022-12-26 12:57:08 +0300

    Update updatebot.sh

--- updatebot.sh
+++ updatebot.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Read the first and second line of the botdata.txt file
-replace_value1=$(head -n 1 data.txt)
-replace_value2=$(tail -n +2 data.txt | head -n 1)
+replace_value1=$(head -n 1 botdata.txt)
+replace_value2=$(tail -n +2 botdata.txt | head -n 1)
 
-# Replace the target value in the Bot.py script with the replacement value
+# Replace the target value in the Bot.py script with the replacement values from botdata.txt
 sed -i "s/replacewithyourbottoken/$replace_value2/g" /Bots/Bot.py 
 sed -i "s/replacewithadminchatid/$replace_value1/g" /Bots/Bot.py
 

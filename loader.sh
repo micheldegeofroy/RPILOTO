1348a473ef337015d9fcd3aff8d8dc6e180161a2
Michel de Geofroy <micheldegeofroy@gmail.com>
2022-12-26 13:22:25 +0300

    Update loader.sh

--- loader.sh
+++ loader.sh
@@ -5,6 +5,37 @@
 # echo -e "pi\npi\n" | sudo passwd root 
 
 # ###############################
+# Get Bot Token and Admin Chat ID
+# ###############################
+
+sudo rm botdata.txt
+sudo rm chat_ids.txt
+
+sudo touch botdata.txt
+sudo touch chat_ids.txt
+
+# Ask the user for telegram  chat ID
+echo -n "What is your telegram Chat ID?: "
+read chat_id
+
+# Ask the user for telegram  Bot Token
+echo -n "What is your telegram Bot Token?: "
+read token
+
+# Store  Chat ID to file
+echo "$chat_id " >> chat_ids.txt
+echo "$chat_id " >> botdata.txt
+echo "$token" >> botdata.txt
+
+# Read the telegram chat ID file and print it
+ID=$(head -n 1 botdata.txt)
+echo "Your Telegram Admin Chat ID is: $ID"
+
+# Read the telegram token from file and print it
+TOKEN=$(tail -n +2 botdata.txt | head -n 1)
+echo "Your Telegram Bot Token is: $TOKEN"
+
+# ###############################
 # Download Install File
 # ###############################
 

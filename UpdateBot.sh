4c23f6f86ab9ddf5f1cdd28d98a2e2bb180610a0
Michel de Geofroy <micheldegeofroy@gmail.com>
2022-12-26 05:23:55 +0300

    Create UpdateBot.sh

---
+++ UpdateBot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Read the first and second line of the botdata.txt file
+replace_value1=$(head -n 1 data.txt)
+replace_value2=$(tail -n +2 data.txt | head -n 1)
+
+# Replace the target value in the Bot.py script with the replacement value
+sed -i "s/replacewithyourbottoken/$replace_value2/g" /Bots/Bot.py 
+sed -i "s/replacewithadminchatid/$replace_value1/g" /Bots/Bot.py
+
+# Confirm that the replacement has been made
+echo "The Admin User Chat ID and Bot Token have been set in Bot.py file."

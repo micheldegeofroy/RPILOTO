3d13e221c30f254c959834be08ac87486a91397a
Michel de Geofroy <micheldegeofroy@gmail.com>
2022-12-31 01:18:30 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -264,13 +264,21 @@ sudo wget "https://raw.githubusercontent.com/micheldegeofroy/RPILOTO/master/bot.
 # Read the first and second line of the botdata.txt file
 replace_value1=$(head -n 1 botdata.txt)
 replace_value2=$(tail -n +2 botdata.txt | head -n 1)
+replace_value2=$(tail -n +3 botdata.txt | head -n 1)
 
 # Replace the target value in the Bot.py script with the replacement values from botdata.txt
 sed -i "s/replacewithyourbottoken/$replace_value2/g" /Bots/Bot.py 
 sed -i "s/replacewithadminchatid/$replace_value1/g" /Bots/Bot.py
+sed -i "s/1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa/$replace_value1/g" /Bots/Bot.py
+
+# Replace the target value in the walletcheck.py script with the replacement values from botdata.txt
+sed -i "s/replacewithyourbottoken/$replace_value2/g" /Bots/walletcheck.py 
+sed -i "s/replacewithadminchatid/$replace_value1/g" /Bots/walletcheck.py 
+sed -i "s/1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa/$replace_value1/g" /Bots/walletcheck.py 
+
 
 # Confirm that the replacement has been made
-echo "The Admin User Chat ID and Bot Token have been set in Bot.py file."
+echo "The Admin User Chat ID, Bot Token and BTC address have been set in Bot.py & walletcheck.py file."
 
 sudo systemctl enable bot.service
 sudo systemctl start bot.service

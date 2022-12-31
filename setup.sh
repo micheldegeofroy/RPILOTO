ff0f7cfe9df1672609549a81ec40b5628fe46b75
Michel de Geofroy <micheldegeofroy@gmail.com>
2022-12-31 16:14:16 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -49,22 +49,22 @@ sudo localedef -f UTF-8 -i en_US en_US.UTF-8
 sudo sed -i 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
 sudo sed -i 's/en_GB.UTF-8 UTF-8/# en_GB.UTF-8 UTF-8/g' /etc/locale.gen
 
-echo " "
-echo " "
-echo "##########################################"
-echo "Update cron Heartbeat"
-echo "##########################################"
-echo " "
-echo " "
+#echo " "
+#echo " "
+#echo "##########################################"
+#echo "Update cron Heartbeat"
+#echo "##########################################"
+#echo " "
+#echo " "
 
 # Read the first and second line of the botdata.txt file
-replace_value2=$(tail -n +2 botdata.txt | head -n 1)
+#replace_value2=$(tail -n +2 botdata.txt | head -n 1)
 
 # Replace the target value in the Bot.py script with the replacement values from botdata.txt
-sed -i "s/replacealsowithyourbottoken/$replace_value2/g" install.sh 
+#sed -i "s/replacealsowithyourbottoken/$replace_value2/g" install.sh 
 
 # Confirm that the replacement has been made
-echo "The cron job Bot Token has been set in install.sh file."
+#echo "The cron job Bot Token has been set in install.sh file."
 
 echo " "
 echo " "

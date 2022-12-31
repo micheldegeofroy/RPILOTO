9a29c3c292b2a6b9a1cc994af53c1f1f75c6fc87
Michel de Geofroy <micheldegeofroy@gmail.com>
2022-12-31 15:50:48 +0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -205,14 +205,13 @@ sudo apt purge python3 -y
 sudo apt purge jq -y
 sudo apt purge pip -y
 
-#sudo rm -r /usr/local/lib/python3.9
+sudo rm -r /usr/local/lib/python3.9
 sudo rm -r /usr/lib/python3
 sudo rm -r file
 #sudo rm -r /usr/lib/php
 #sudo rm -r /usr/lib/apache2
 #sudo rm -r /usr/lib/python2.7
 #sudo rm -r /usr/lib/python3.9
-sudo rm -r/usr/local/lib/python3.9/
 
 sudo apt purge -y
 sudo apt autoremove -y

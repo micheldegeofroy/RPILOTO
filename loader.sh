ee4f4f76cc911e119a7c34e5facc575cfa4f959f
Michel de Geofroy <micheldegeofroy@gmail.com>
2022-12-31 16:15:17 +0300

    Update loader.sh

--- loader.sh
+++ loader.sh
@@ -108,6 +108,35 @@ echo "The cron job Bot Token has been set in install.sh file."
 echo " "
 echo " "
 echo "##########################################"
+echo "Fix Language Local"
+echo "##########################################"
+echo " "
+echo " "
+
+#sudo rm /etc/environment
+sudo touch /etc/environment
+
+sudo echo "LANGUAGE=en_US" | sudo tee -a /etc/environment
+sudo echo "LC_ALL=en_US" | sudo tee -a /etc/environment
+sudo echo "LANG=en_US" | sudo tee -a /etc/environment
+sudo echo "LC_TYPE=en_US" | sudo tee -a /etc/environment
+
+sudo rm /etc/default/locale
+sudo touch /etc/default/locale
+
+sudo echo "LANG=en_US.UTF-8" | sudo tee -a /etc/default/locale
+sudo echo "LC_CTYPE=en_US.UTF-8" | sudo tee -a /etc/default/locale
+sudo echo "LC_MESSAGES=en_US.UTF-8" | sudo tee -a /etc/default/locale
+sudo echo "LC_ALL=en_US.UTF-8" | sudo tee -a /etc/default/locale
+
+sudo localedef -f UTF-8 -i en_US en_US.UTF-8 
+
+sudo sed -i 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
+sudo sed -i 's/en_GB.UTF-8 UTF-8/# en_GB.UTF-8 UTF-8/g' /etc/locale.gen
+
+echo " "
+echo " "
+echo "##########################################"
 echo "Cron @ Reboot to Launch Install"
 echo "##########################################"
 echo " "

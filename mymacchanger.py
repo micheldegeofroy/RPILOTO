16cb4cde1a445c599eccd7d8c1409fd9c906af07
Michel de Geofroy <micheldegeofroy@gmail.com>
2023-01-02 15:17:52 +0300

    Update mymacchanger.py

--- mymacchanger.py
+++ mymacchanger.py
@@ -1,8 +1,17 @@
 import random
 import subprocess
-random_mac = [random.randint(0x00, 0xff) for _ in range(6)]
-new_mac = ':'.join(map(lambda x: '%02x' % x, random_mac))
+
+random_mac_wlan0 = [random.randint(0x00, 0xff) for _ in range(6)]
+new_mac_wlan0 = ':'.join(map(lambda x: '%02x' % x, random_mac_wlan0))
 subprocess.call("ifconfig wlan0 down", shell=True)
-subprocess.call("ifconfig wlan0 hw ether " + new_mac, shell=True)
+subprocess.call("ifconfig wlan0 hw ether " + new_mac_wlan0, shell=True)
 subprocess.call("ifconfig wlan0 up", shell=True)
-print("New Mac address: " + new_mac)
+
+random_mac_eth0 = [random.randint(0x00, 0xff) for _ in range(6)]
+new_mac_eth0 = ':'.join(map(lambda x: '%02x' % x, random_mac_eth0))
+subprocess.call("ifconfig eth0 down", shell=True)
+subprocess.call("ifconfig eth0 hw ether " + new_mac_eth0, shell=True)
+subprocess.call("ifconfig eth0 up", shell=True)
+
+print("New Mac address wlan0: " + new_mac_wlan0)
+print("New Mac address eth0: " + new_mac_eth0)
